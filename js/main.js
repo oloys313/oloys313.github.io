0e94ba6be3011b8b11df73cd551fe042c8eccff7
oloys010 <91203160+oloys010@users.noreply.github.com>
2022-02-09 22:25:44 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { OBJLoader } from 'OBJLoader.js';
+import { OBJLoader } from '/OBJLoader.js';
 const scene = new THREE.Scene();
 		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
