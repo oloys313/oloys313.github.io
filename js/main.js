cd8d8d28982321806a23b40ac7484fd37b7f1325
oloys010 <91203160+oloys010@users.noreply.github.com>
2022-02-09 22:03:31 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { OBJLoader } from 'OBJLoader.js';
 const scene = new THREE.Scene();
 		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -88,6 +89,32 @@ const scene = new THREE.Scene();
 
 		document.body.onscroll = moveCamera;
 		moveCamera();
+
+		const loader = new OBJLoader();
+
+		// load a resource
+		loader.load(
+			// resource URL
+			'models/monster.obj',
+			// called when resource is loaded
+			function ( object ) {
+
+				scene.add( object );
+
+			},
+			// called when loading is in progresses
+			function ( xhr ) {
+
+				console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+			},
+			// called when loading has errors
+			function ( error ) {
+
+				console.log( 'An error happened' );
+
+			}
+		);
 
 				
 				function animate() {
