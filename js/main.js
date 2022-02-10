6f3065ce935df2082fdb8a50aea5a64064180d7c
LordShadow-itch-io <samueloloy13@gmail.com>
2022-02-10 20:35:23 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //import { OBJLoader } from '/OBJLoader.js';
 
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/three@<version>';
 
 const scene = new THREE.Scene();
 	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
