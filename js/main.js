fea39fc078010518c0ee138d2815c7401042962f
LordShadow-itch-io <samueloloy13@gmail.com>
2022-02-10 21:02:45 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@ const scene = new THREE.Scene();
 	document.body.onscroll = moveCamera;
 	moveCamera();
 
-	/*const objloader = new THREE.OBJLoader();
+	const objloader = new THREE.OBJLoader();
 
 	// load a resource
 	objloader.load(
@@ -117,7 +117,7 @@ const scene = new THREE.Scene();
 			console.log( 'An error happened' );
 
 		}
-	);*/
+	);
 
 			
 			function animate() {
