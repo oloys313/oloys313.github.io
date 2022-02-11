12d6f516442e6d035f4cc435e60024ec29f4994c
LordShadow-itch-io <samueloloy13@gmail.com>
2022-02-11 17:43:19 +0000

    update main.js and index.html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-const { MeshBasicMaterial } = require("./three");
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const loader = new THREE.TextureLoader();
+const bgTexture = loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/daikanyama.jpg');
+scene.background = bgTexture;
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
@@ -44,12 +46,50 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
+const jeffTexture = new THREE.TextureLoader().load('img/jeff.png');
 const jeff = new THREE.Mesh(
     new THREE.BoxGeometry(3,3,3),
-    new THREE.MeshBasicMaterial({color:0x00ff00}),
+    new THREE.MeshStandardMaterial({map: jeffTexture,}),
 );
 scene.add(jeff);
 
+const moonTexture = new THREE.TextureLoader().load('img/jeff.png');
+
+const moon = new THREE.Mesh(
+new THREE.SphereGeometry(3, 32, 32),
+new THREE.MeshStandardMaterial({
+    map: moonTexture,
+})
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+jeff.position.z = -5;
+jeff.position.x = 2;
+
+// Scroll Animation
+
+function moveCamera() {
+const t = document.body.getBoundingClientRect().top;
+moon.rotation.x += 0.05;
+moon.rotation.y += 0.075;
+moon.rotation.z += 0.05;
+
+jeff.rotation.y += 0.01;
+jeff.rotation.z += 0.01;
+
+camera.position.z = t * -0.01;
+camera.position.x = t * -0.0002;
+camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+        
 function animate() {
     requestAnimationFrame(animate);
 
