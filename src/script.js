2c9e1e9d6f6cf76519dd82e48a2bc24cb04a870c
LordShadow-itch-io <samueloloy13@gmail.com>
2022-02-12 20:55:01 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import * as THREE from 'https://cdn.skypack.dev/-/three@v0.137.5-HJEdoVYPhjkiJWkt6XIa/dist=es2019,mode=raw/build/three.module.js'
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+//import * as dat from 'dat.gui'
 
 //Loading
 const textureLoader = new THREE.TextureLoader()
