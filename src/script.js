a7fc64fb6bf8944b6dbcd51b70846ea12c2c3df7
oloys313 <91203160+oloys313@users.noreply.github.com>
2022-02-13 15:28:50 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ import * as THREE from 'https://cdn.skypack.dev/-/three@v0.137.5-HJEdoVYPhjkiJWk
 //Loading
 const textureLoader = new THREE.TextureLoader()
 
-const normalTexture = textureLoader.load('https://oloys313.github.io/static//textures/11972-normal.jpg')
+const normalTexture = textureLoader.load('https://oloys313.github.io/static//textures/GlobeNormal.png')
 
 // Debug
 //const gui = new dat.GUI()
