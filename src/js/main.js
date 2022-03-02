f9575865a18eb1c82f0da8b9fd6dc9dec27e2d9c
oloys313 <91203160+oloys313@users.noreply.github.com>
2022-03-02 19:05:05 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -69,4 +69,29 @@ document.addEventListener('DOMContentLoaded', () => {
                                         .setPin("#trigger1")
                                         //.addIndicators() // add indicators (requires plugin)
                                         .addTo(controller3);
+    
+    var ctrl = new ScrollMagic.Controller();
+
+// Create scenes in jQuery each() loop
+$("section").each(function(i) {
+  var inner = $(this).find(".inner");
+  var outer = $(this).find(".outer");
+  var tl = new TimelineMax();
+  
+  tl.from(outer, 0.25, { scaleX: 0 });
+  tl.from(inner, 0.65, { yPercent: 100, ease: Back.easeOut });
+  
+  new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.15
+  })
+    .setTween(tl)
+    .addIndicators({
+      colorTrigger: "white",
+      colorStart: "white",
+      colorEnd: "white",
+      indent: 40
+    })
+    .addTo(ctrl);
+});
 })
